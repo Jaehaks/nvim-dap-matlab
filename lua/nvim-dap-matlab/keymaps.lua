824974ffd76f3658c0025d67ea3db8fd7078e6ab
Jaehaks <wogkr3654@naver.com>
2026-02-28 16:35:22 +0900

    fix : set gui_windows keymap for debugging buffer only.
    1-1. Problem
    
    While debug session, keymaps of gui_windows are created but these are
    set for all opened buffers because we don't specify buffer option.
    
    It needs to be limited to the matlab file only.
    
    1-2. Problem
    
    The created keymaps are applied to only a buffer which start debug
    session. If I move focus to other matlab file, the shortcuts don't work.
    
    2-1, 2-2, Solution
    
    Matlab files are listed and set keymaps for them while debug session.
    If new matlab file is opened, it has also the keymaps from autocmds

--- lua/nvim-dap-matlab/keymaps.lua
+++ lua/nvim-dap-matlab/keymaps.lua
@@ -38,25 +38,60 @@ end
 ---@param dap table
 ---@param opts dap_matlab.config
 M.set_keymaps = function(dap, opts)
-	if opts.gui_windows.keymaps.toggle_workspace then
-		vim.keymap.set('n', opts.gui_windows.keymaps.toggle_workspace, function () toggle_workspace(dap) end,
-		{desc = '[matlab-dap] Toggle workspace window'})
+
+	-- set keymaps as low level function
+	---@param bufnr number
+	local function _set_keymaps(bufnr)
+		if opts.gui_windows.keymaps.toggle_workspace then
+			vim.keymap.set('n', opts.gui_windows.keymaps.toggle_workspace, function () toggle_workspace(dap) end,
+			{desc = '[matlab-dap] Toggle workspace window', buffer = bufnr, silent = true})
+		end
+		if opts.gui_windows.keymaps.toggle_filebrowser then
+			vim.keymap.set('n', opts.gui_windows.keymaps.toggle_filebrowser, function () toggle_filebrowser(dap) end,
+			{desc = '[matlab-dap] Toggle file browser window', buffer = bufnr, silent = true})
+		end
 	end
-	if opts.gui_windows.keymaps.toggle_filebrowser then
-		vim.keymap.set('n', opts.gui_windows.keymaps.toggle_filebrowser, function () toggle_filebrowser(dap) end,
-		{desc = '[matlab-dap] Toggle file browser window'})
+
+	-- set keymaps for all opened matlab buffer
+	for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].filetype == 'matlab' then
+			_set_keymaps(bufnr)
+		end
 	end
+
+	-- set keymaps for new opening matlab buffer
+	vim.api.nvim_create_augroup('matlab-dap-gui-windows', {clear = true})
+	vim.api.nvim_create_autocmd('FileType', {
+		group = 'matlab-dap-gui-windows',
+		pattern = 'matlab',
+		callback = function (args)
+			_set_keymaps(args.buf)
+		end
+	})
 end
 
 --- delete keymaps for matlab debugging
 ---@param opts dap_matlab.config
 M.del_keymaps = function(opts)
-	if opts.gui_windows.keymaps.toggle_workspace then
-		pcall(vim.keymap.del, 'n', opts.gui_windows.keymaps.toggle_workspace)
+
+	-- delete keymaps as low level function
+	---@param bufnr number
+	local function _del_keymaps(bufnr)
+		if opts.gui_windows.keymaps.toggle_workspace then
+			pcall(vim.keymap.del, 'n', opts.gui_windows.keymaps.toggle_workspace, {buffer = bufnr})
+		end
+		if opts.gui_windows.keymaps.toggle_filebrowser then
+			pcall(vim.keymap.del, 'n', opts.gui_windows.keymaps.toggle_filebrowser, {buffer = bufnr})
+		end
 	end
-	if opts.gui_windows.keymaps.toggle_filebrowser then
-		pcall(vim.keymap.del, 'n', opts.gui_windows.keymaps.toggle_filebrowser)
+
+	-- delete keymaps for all opened matlab buffer
+	for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].filetype == 'matlab' then
+			_del_keymaps(bufnr)
+		end
 	end
+	pcall(vim.api.nvim_clear_autocmds, {group = 'matlab-dap-gui-windows' })
 end
 
 return M
